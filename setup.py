6b5581865e356fb3819314ba82c7f8db6abe7c36
Karan Joshi <joshikaran096@gmail.com>
2025-07-04 14:34:12 +0530

    version changed to 4.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="backtestTools",
-    version="4.0.2",
+    version="4.0.3",
     author="Mudraksh",
     author_email="contact.mudraksh@gmail.com",
     description="BacktestTools",
